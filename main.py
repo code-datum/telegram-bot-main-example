83aabe4eaff79a949e977a74d18db2ed12c374f6
Maxat <code.datum@gmail.com>
2020-02-24 22:52:43 +0600

    Set base template

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# 1 Import the libraries
-from telegram.ext import Updater, CommandHandler
-import requests
-import logging
-import os
+import logging, os
 
-logging.basicConfig(level=logging.DEBUG)
-import re
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 
-TOKEN_FILE = 'private_keys/RandomDogExBot.token'
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
+logger = logging.getLogger(__name__)
 
-# 2 Asses the API andd get the image URL
-# {"fileSizeBytes":104652,"url":"https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg"}
-# contents = requests.get('https://random.dog/woof.json').json()
-# image_url = contents['url']
-# logging.debug("image_url%s ", image_url)
+TOKEN_FILE = 'private_keys/telegram.token'
 
 def get_token(filename):
     if os.path.exists(filename):
@@ -28,35 +24,46 @@ def get_token(filename):
     return token
 
 
-def get_url():
-    '''
-    get the image URL
-    :return string:
-    '''
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    logging.debug("image_url%s %s ", url, 'get_url')
-    return url
+def start(update, context):
+    update.message.reply_text("It is a simple currency convector")
 
 
-def bop(bot, update):
-    '''
-    # 3 send the image
-    :param bot:
-    :param update:
-    :return void:
-    '''
-    url = get_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye!.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
     TOKEN = get_token(TOKEN_FILE)
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop', bop))
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('cancel', cancel))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
