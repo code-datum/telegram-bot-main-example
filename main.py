1b38bf5f83877c830d27695e4f1acebbd8b67b83
Maxat <code.datum@gmail.com>
2020-02-24 23:26:31 +0600

    Add basic commands

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging, os
-
+import logging
+import os
+import requests
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
@@ -11,6 +12,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 TOKEN_FILE = 'private_keys/telegram.token'
+BASE_API_URL = 'https://api.exchangeratesapi.io'
+
 
 def get_token(filename):
     if os.path.exists(filename):
@@ -24,8 +27,58 @@ def get_token(filename):
     return token
 
 
+# Helper functions
+def get_latest_currency_api(url, code):
+    prefix_url = '/latest?base='+code
+    content = requests.get(url=url+prefix_url).json()
+    logger.debug('get latest currency from %s: %s', url, content)
+    return content
+
+
+# End helper functions
+# Telegram functions
 def start(update, context):
     update.message.reply_text("It is a simple currency convector")
+
+
+def list(update, context):
+    '''
+    # /list, /lst returns list of all available rates from: https://api.exchangeratesapi.io/latest?base=USD
+    :param update:
+    :param contexct:
+    :return:
+    '''
+    currency = get_latest_currency_api(BASE_API_URL, 'USD')
+    if not currency:
+        update.message.reply_text("list got it")
+    else:
+        update.message.reply_text("Service has some errors!")
+    user = update.message.from_user
+    logger.info("User %s start use 'list' command.", user.first_name)
+
+
+def exchange(update, context):
+    '''
+    $10 to CAD or  /exchange 10 USD to CAD - converts to the second currency with two decimal precision and return.
+    Ex.:  $15.55
+    :param update:
+    :param context:
+    :return:
+    '''
+    user = update.message.from_user
+    logger.info("User %s start use 'exchange' command.", user.first_name)
+
+
+def history(update, context):
+    '''
+    USD/CAD for 7 days - return an image graph chart which shows the exchange rate graph/chart
+    of the selected currency for the last 7 days
+    :param update:
+    :param context:
+    :return:
+    '''
+    user = update.message.from_user
+    logger.info("User %s start use 'history' command.", user.first_name)
 
 
 def cancel(update, context):
@@ -42,6 +95,8 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+# End telegram functions
+
 def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
@@ -53,7 +108,10 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('cancel', cancel))
+    dp.add_handler(CommandHandler(['list', 'lst'], list))
+    dp.add_handler(CommandHandler('exchange', exchange))
+    dp.add_handler(CommandHandler('history', history))
+    dp.add_handler(CommandHandler(['cancel', 'exit', 'quit'], cancel))
 
     # log all errors
     dp.add_error_handler(error)
